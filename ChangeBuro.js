6b0260583cad823f32a0dfedd87145ec30a774dd
banko2 <60343173+banko2@users.noreply.github.com>
2022-11-27 08:47:41 +0200

    Add files via upload

---
+++ ChangeBuro.js
@@ -0,0 +1,12 @@
+sum = () => {
+    let num1 = Number(window.prompt('Input first digit'));
+    let num2 = Number(window.prompt('Input second digit'));
+    let sum1 = num1 + num2;
+    //return sum1;
+
+    window.alert('Sum is ' + sum1);
+    document.write('Sumata e ' + sum1);
+};
+//document.write("<h2 style='color:blue'>JS rules</h2>");
+//document.write("<hr/>");
+//document.write(sum());
